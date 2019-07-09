8714d583b4eeaab316aa2615aa42bb3dd38908a7
Jóvenes Construyendo el Futuro 07 <jovenes.cf07@conacyt.mx>
2019-07-09 11:31:45 -0500

    se crearon cambios

---
+++ src/components/WeatherTemperature.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>icono</div>
+    <div>12° C</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "WeatherTemperature",
+  props: {
+    msg: String
+  }
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import WeatherTemperature from "./components/WeatherTemperature";
 
 Vue.use(Router);
-
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
@@ -21,6 +21,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/WeatherTemperature",
+      name: "WeatherTemperature",
+      component: WeatherTemperature
     }
   ]
 });
