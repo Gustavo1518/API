0a7c0a54ec49f054091ded2409cd46b2b2adf74b
Jóvenes Construyendo el Futuro 19 <jovenes.cf19@conacyt.mx>
2019-07-09 11:43:35 -0500

    commit 2

---
+++ src/components/WeatherExtraInfo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>porcentaje</div>
+    <div>velocidad</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
