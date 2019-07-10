0d51d17f45c7a64e61ff8e3cbdcbf48bf0a811c3
Jóvenes Construyendo el Futuro 19 <jovenes.cf19@conacyt.mx>
2019-07-10 11:00:46 -0500

    8 commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,12 +9,39 @@
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 import WeatherLocation from "@/components/WeatherLocation";
-
+import axios from "axios";
 export default {
   name: "home",
   components: {
     HelloWorld,
     WeatherLocation
+  },
+  created() {
+    this.ciudades();
+  },
+  methods: {
+    ciudades() {
+      var key = "521a97605aaf878aceadfa493caa3bfb";
+      axios
+        .get(
+          "http://api.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=" +
+            key,
+          {
+            method: "GET",
+            mode: "no-corse",
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Content-Type": "aplication/json"
+            }
+          }
+        )
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
   }
 };
 </script>
